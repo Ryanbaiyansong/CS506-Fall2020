65e71978df52f9ca78f36634de7f207e13323df6
Yansong Bai <baibest1@bu.edu>
2020-09-13 15:45:44 -0400

    added outdoors files including lake and park

--- 01-python/outdoors/park.py
+++ 01-python/outdoors/park.py
@@ -1,3 +1,25 @@
 def draw_park():
-    print("park not found")
+    print("             |||               _____    ")
+    print("           (  0   )           (^ o ^)")
+    print("         (          )       // (   )\\  ")
+    print("        (            )          ^^ 
+    print("      (               )                              ")
+    print("      (     0           )                          ")
+    print("     (             0     )        |________________|                ")
+    print("   (                      )       |                |")
+    print(" (  0                      )      |     chair      |                ")
+    print("  (                         )     |                | ")
+    print(" (                0         )    |__________________|")
+    print(" (                         )     ||||||||||||||||||||")
+    print("  (                    )         ||||||||||||||||||||                     ")
+    print("   VVVVVVVVVVVVVVVVVVVVVVV                             ")
+    print("           /||||\                    o                 ")
+    print("          /||||| \                  / \                  ")
+    print("         /||||||||\              ccccccco               ")
+    print("        /||||||||||\             ( ^ c ^ )              ")
+    print("       /||||||||||||\             (||:||)             ")
+    print("      /||||||||||||||\             || ||              ")
+    print("                                  (_) (_)  ")
+
+
     return
