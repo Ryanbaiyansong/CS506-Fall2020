4f7413abcd0d7047358b65dc4d549089209c4168
Yansong Bai <baibest1@bu.edu>
2020-09-13 15:45:44 -0400

    added outdoors files including lake and park
    add birds to the park
    
    add birds as parameter
    
    add birds as parameter inclusing for loop to print
    
    add default to outdoors.py
    
    add default values to outdoors.py
    
    Errors Correction

--- 01-python/outdoors/park.py
+++ 01-python/outdoors/park.py
@@ -1,3 +1,28 @@
-def draw_park():
-    print("park not found")
+def draw_park(birds = 3):
+    print("             |||               _____    ")
+    print("           (  0   )           (^ o ^)")
+    print("         (          )       // (   )\\  ")
+    print("        (            )          ^^ ")
+    print("      (               )                              ")
+    print("      (     0           )                          ")
+    print("     (             0     )        |________________|                ")
+    print("   (                      )       |                |")
+    print(" (  0                      )      |     chair      |                ")
+    print("  (                         )     |                | ")
+    print(" (                0         )    |__________________|")
+    print(" (                         )     ||||||||||||||||||||")
+    print("  (                    )         ||||||||||||||||||||                     ")
+    print("   VVVVVVVVVVVVVVVVVVVVVVV                             ")
+    print("           /||||\                    o                 ")
+    print("          /||||| \                  / \                  ")
+    print("         /||||||||\              ccccccco               ")
+    print("        /||||||||||\             ( ^ c ^ )              ")
+    print("       /||||||||||||\             (||:||)             ")
+    print("      /||||||||||||||\             || ||              ")
+    print("                                  (_) (_)  ")
+    for i in birds:
+        print("(o o)   (o o)")
+        print("(  V  ) (  V  ) ")
+        print("/--m-m- /--m-m-")
+
     return
