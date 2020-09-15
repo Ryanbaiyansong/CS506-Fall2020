e1ea4eb4062908d887f4265e525ddd6e0d4897dd
Yansong Bai <baibest1@bu.edu>
2020-09-15 11:33:07 -0400

    initial try

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,12 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    res = [] #matrix to store the result
+    with open(csv_file_path) as csvDataFile:
+        rows = csvDataFile.readlines()
+
+    for row in rows:
+        data = row.strip().split(",") # split by comma
+        res.append([eval(val) for val in data])
+            
+    return res
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,44 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    """
+    Calculate the euclidean distance between x and y
+    """   
+    return (sum([(a - b) ** 2 for a, b in zip(x, y)]))**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    """
+    Calculate the manhattan distance between x and y
+    """ 
+    return sum(abs(a-b) for a,b in zip(x,y))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    """
+    Calculate the jaccard distance between x and y
+    """
+    if len(x) == 0 or len(y) == 0:
+        raise ValueError("lengths must not be zero")
+    x_set = set(x)
+    y_set = set(y)
+    intersection = len(x_set.intersection(y_set))
+    union = len(x_set.union(y_set))
+    return 1 - intersection/union
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    """
+    Calculate the cosine similarity between x and y
+    """
+    if len(x) == 0 or len(y) == 0:
+        raise ValueError("lengths must not be zero")
+    if len(x) != len(y):
+        raise ValueError("lengths must be equal")
+    dot = 0
+    x_magnitude = 0
+    y_magnitude = 0
+    for i in range(len(x)):
+        dot += x[i] * y[i]
+        x_magnitude += x[i]**2
+        y_magnitude += y[i]**2
+    x_magnitude = x_magnitude**(1/2)
+    y_magnitude = y_magnitude**(1/2)
+    return dot / (x_magnitude * y_magnitude)
 
 # Feel free to add more
